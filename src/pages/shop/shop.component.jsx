08260ab3e4e7f789f857593bfdaec17fdb875f7a
Kuldeep Singh <ks.genex@gmail.com>
2021-10-11 13:56:47 +0530

    moved the state from component to the reducer and added selector for the same.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,22 +1,16 @@
 import React from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
+import { Route } from "react-router-dom";
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
-import { selectCollectionSections } from "../../redux/shop/shop.selector";
+import CollectionOverview from "../../components/collection-overview/collection-overview.component";
+import CollectionPage from "../collection/collection.component";
 
-const ShopPage = ({ collections }) => {
+const ShopPage = ({ match }) => {
   return (
     <div className="shop-page">
-      {collections.map(({ id, ...otherProps }) => {
-        return <CollectionPreview key={id} {...otherProps} />;
-      })}
+      <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
   );
 };
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollectionSections,
-});
-
-export default connect(mapStateToProps, null)(ShopPage);
+export default ShopPage;
