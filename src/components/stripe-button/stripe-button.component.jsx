c5ffd9549db157b68ae0fc182c6d3ed4d72e4777
Kuldeep Singh <ks.genex@gmail.com>
2021-10-25 13:03:49 +0530

    MVP done

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51JjKBJSG1Q3BlNFxRStkU7JjQdDDaPn2zTb2ybbL56VuxaBdPtm1RJI5h3d2tCySrb6TVTRdncq4EUMcpODzQAJg00KSmRfq10";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Crwn Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is ${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
